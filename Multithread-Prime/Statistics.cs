534fd2a26ed3a378035aba7b22122129a3d990d1
Ismael <ismael.em@outlook.com>
2020-10-30 12:34:49 +0200

    removed async Statistics

--- Multithread-Prime/Statistics.cs
+++ Multithread-Prime/Statistics.cs
@@ -10,37 +10,37 @@ namespace Multithread_Prime {
         private int _maxThreads = 2;
         internal int MaxThreads {
             get { return _maxThreads; }
-            set { _maxThreads = value; NotifyPropertyChangedAsync("MaxThreads"); }
+            set { _maxThreads = value; NotifyPropertyChanged("MaxThreads"); }
         }
 
         private int _maxPrime = 0;
         internal int MaxPrime {
             get { return _maxPrime; }
-            set { _maxPrime = value; NotifyPropertyChangedAsync("MaxPrime"); }
+            set { _maxPrime = value; NotifyPropertyChanged("MaxPrime"); }
         }
 
         private int _minPrime = 999999999;
         internal int MinPrime {
             get { return _minPrime; }
-            set { _minPrime = value; NotifyPropertyChangedAsync("MinPrime"); }
+            set { _minPrime = value; NotifyPropertyChanged("MinPrime"); }
         }
 
         private int _processed = 0;
         internal int Processed {
             get { return _processed; }
-            set { _processed = value; NotifyPropertyChangedAsync("Processed"); }
+            set { _processed = value; NotifyPropertyChanged("Processed"); }
         }
 
-        private string _lastFile = "";
+        private string _lastFile = "_";
         internal string LastFile {
             get { return _lastFile; }
-            set { _lastFile = value; NotifyPropertyChangedAsync("LastFile"); }
+            set { _lastFile = value; NotifyPropertyChanged("LastFile"); }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        public async Task NotifyPropertyChangedAsync([CallerMemberName] string propertyName = "") {
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
             if (PropertyChanged != null) {
-                await Dispatcher.RunAsync(CoreDispatcherPriority.High, () => {
+                 var _ = Dispatcher.RunAsync(CoreDispatcherPriority.High, () => {
                     PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
                 });
             }
